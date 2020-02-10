dd2b5eedae15947b29d686cc8c331904f1553116
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-02-10 23:07:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
   $load = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
-  $action = (isset($_GET['action']) && $_GET['action'] != '') ? $_GET['action'] : '';
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,24 +11,24 @@
   <body>
     <div class="sidenav">
       <img src="img/icon.png">
-      <a href="index.php?page=home">Home</a>
-      <a href="index.php?page=vfood">Vegan Foods</a>
-      <a href="index.php?page=vnews">Vegan News</a>
+      <a href="veganlifestyle.herokuapp.com?page=home">Home</a>
+      <a href="veganlifestyle.herokuapp.com?page=vfood">Vegan Foods</a>
+      <a href="veganlifestyle.herokuapp.com?page=vnews">Vegan News</a>
     </div>
     <div class="content">
-			<?php
-			switch ($load) {
-				case 'vfood':
-				    require_once('page-food.php');
-				        break;
-				case 'vnews':
-			      require_once('page-news.php');
-			          break;
-				default:
-					  require_once('home.php');
-					      break;
-				}
-			?>
+	<?php
+	  switch ($load) {
+		case 'vfood':
+	    		require_once('veganlifestyle.herokuapp.com/page-food.php');
+			break;
+		case 'vnews':
+			require_once('veganlifestyle.herokuapp.com/page-news.php');
+			break;
+		default:
+			require_once('veganlifestyle.herokuapp.com/home.php');
+			break;
+	   }
+	?>
     </div>
   </body>
 </html>
