c412eabbb36b65f506ea32cb9b463d4166568bdf
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-02-10 23:12:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,24 +11,24 @@
   <body>
     <div class="sidenav">
       <img src="img/icon.png">
-      <a href="?page=home">Home</a>
-      <a href="?page=vfood">Vegan Foods</a>
-      <a href="?page=vnews">Vegan News</a>
+      <a href="index.php?page=home">Home</a>
+      <a href="index.php?page=vfood">Vegan Foods</a>
+      <a href="index.php?page=vnews">Vegan News</a>
     </div>
     <div class="content">
-	<?php
-	  switch ($load) {
-		case 'vfood':
-	    		require_once('veganlifestyle.herokuapp.com/page-food.php');
-			break;
-		case 'vnews':
-			require_once('veganlifestyle.herokuapp.com/page-news.php');
-			break;
-		default:
-			require_once('veganlifestyle.herokuapp.com/home.php');
-			break;
-	   }
-	?>
+			<?php
+			switch ($load) {
+				case 'vfood':
+				    require_once('page-food.php');
+				        break;
+				case 'vnews':
+			      require_once('page-news.php');
+			          break;
+				default:
+					  require_once('home.php');
+					      break;
+				}
+			?>
     </div>
   </body>
 </html>
